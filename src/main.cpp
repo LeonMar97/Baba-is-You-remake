2711d6c4a57fe918505a9861ccd23a2582cab0ad
ronehe <Ro.Heifetz@gmail.com>
2021-05-31 15:42:31 +0300

    baba now closes legs

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,11 @@
 
 int main()
 {
-    sf::RectangleShape player1(sf::Vector2f(45.f, 45.f));
-    sf::RectangleShape player2(sf::Vector2f(45.f, 45.f));
+    sf::RectangleShape player(sf::Vector2f(40.f, 45.f));
     auto tex = sf::Texture();
     tex.loadFromFile("Baba.PNG");
-    player1.setTexture(&tex);
-    player2.setTexture(&tex);
-    player2.setPosition(100, 100);
-   Animation animation1(&tex, sf::Vector2u(3, 2), 0.2f);
-   Animation animation2(&tex, sf::Vector2u(3, 2), 0.2f);
+    player.setTexture(&tex);
+   Animation animation(&tex, sf::Vector2u(3, 3), 0.2f);
     float deltaTime = 0.0f;
     sf::Clock clock;
     auto window = sf::RenderWindow(sf::VideoMode(300, 300), "Example");
@@ -26,20 +22,17 @@ int main()
             switch (event.type)
             {
             case sf::Event::KeyPressed:
-                if (event.key.code == sf::Keyboard::K) player1.move(sf::Vector2f(1, 0));
+                if (event.key.code == sf::Keyboard::K) player.move(sf::Vector2f(1, 0));
 			break;
             case sf::Event::Closed:
                 window.close();
                 break;
             }
         }
-		animation1.update(1, deltaTime);
-		player1.setTextureRect(animation1.texRect);
-		animation2.update(0, deltaTime);
-		player2.setTextureRect(animation2.texRect);
+		animation.update(2, deltaTime);
+		player.setTextureRect(animation.texRect);
         window.clear();
-        window.draw(player1);
-        window.draw(player2);
+        window.draw(player);
         window.display();
     }
 }
