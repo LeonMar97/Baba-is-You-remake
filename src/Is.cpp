52771692c01248899a7eebdb70a3c5c892542ce6
Leon Markovich <leonmarkovich97@gmail.com>
2021-06-03 17:00:42 +0300

    changed is color to gray

--- src/Is.cpp
+++ src/Is.cpp
@@ -3,6 +3,7 @@ Is::Is(const sf::Vector2u& loc)
 	:Conjunction("is",Textures::instance().get_Textures(is_t), DEFAULT_IMG_COUNT, loc)
 	
 {
-	m_character.setOutlineThickness(1);
-	m_character.setOutlineColor(sf::Color::Red);
+	m_character.setFillColor(sf::Color(0x80, 0x80, 0x80));
+	//m_character.setOutlineThickness(1);
+	//m_character.setOutlineColor(sf::Color::Red);
 }
