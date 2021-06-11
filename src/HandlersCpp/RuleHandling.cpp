97679c2960b7b11f2b88feb1058c62e4f42dca4f
Leon Markovich <leonmarkovich97@gmail.com>
2021-06-11 14:27:34 +0300

    started working on creating rules and adding them to the vec

--- src/HandlersCpp/RuleHandling.cpp
+++ src/HandlersCpp/RuleHandling.cpp
@@ -15,8 +15,8 @@ void  RuleHandling::processCollision(BaseObject& object1, BaseObject& object2, B
 
 RuleToFunctionMap RuleHandling:: initializeCollisionMap(){
     RuleToFunctionMap map;
-    map[Key( typeid(Noun), typeid(Conjunction), typeid(Attribute) )] = &NounConAtr;
-    map[Key( typeid(Noun), typeid(Conjunction), typeid(Noun)      )] = &NounConNoun;
+    map[Key( typeid(Noun), typeid(Conjunction), typeid(Attribute) )] = &updateRules;
+    map[Key( typeid(Noun), typeid(Conjunction), typeid(Noun)      )] = &updateRules;
     return map;
 }
 
@@ -28,3 +28,45 @@ FunctionPtr RuleHandling::lookup(const std::type_index& class1, const std::type_
     }
     return mapEntry->second;
 }
+
+
+void Board::replace(GameObjects objectToAdd, GameObjects objectToRemove, char objectToCreate) {
+	for (auto& removeObj : m_dataHolder[objectToRemove]) {
+		auto removeObjPos = removeObj->returnPos() / OBJECT_SIZE;
+		addGameObj(objectToCreate, sf::Vector2u(removeObjPos.y, removeObjPos.x));
+		delete removeObj;
+	}
+	m_dataHolder[objectToRemove].clear();
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+
+/// <summary>
+/// creating a temporary new rule set and updates the current rule set dynamically
+/// </summary>
+/// <param name="newRules"></param>
+void Board::updateRules(std::vector<ruleTuple>& newRules) {
+	bool ruleAlreadyExists = false;
+	for (auto ruleIndex = 0; ruleIndex < m_Rules.size(); ruleIndex++) {
+		for (auto newRuleIndex = 0; newRuleIndex < newRules.size(); newRuleIndex++) {
+			if (m_Rules[ruleIndex] == newRules[newRuleIndex]) {
+				newRules.erase(newRules.begin() + newRuleIndex); //remove new rule because it already exists
+				ruleAlreadyExists = true;
+				break;
+			}
+		}
+		if (!ruleAlreadyExists) {
+			if (auto atrPtr = dynamic_cast<Attribute*>(&(std::get<2>(m_Rules[ruleIndex])))) {
+				std::get<0>(m_Rules[ruleIndex]).removeAttributes(atrPtr);
+				m_Rules.erase(m_Rules.begin() + ruleIndex); //remove old rule because it is no longer on map
+			}
+		}
+	}
+	for (auto& newRule : newRules) {
+		std::get<2>(newRule).putRuleIntoAffect(std::get<0>(newRule), *this);
+	}
+}
+
+
